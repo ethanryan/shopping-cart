42af71fddb1e6126777d6147e8051f09426dbf9b
ethanryan <ethanryan@gmail.com>
2018-10-09 22:47:34 -0400

    adding state to ShoppingCartContainer

--- src/container/ShoppingCartContainer.js
+++ src/container/ShoppingCartContainer.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 import ItemList from '../components/ItemList';
 
 class ShoppingCartContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      message: 'helloooooo everybody!',
+      something: true,
+    }
+  }
+
   render() {
+    console.log('ShoppingCartContainer, this.state is: ', this.state)
     return (
       <div className="ShoppingCartContainer">
 
