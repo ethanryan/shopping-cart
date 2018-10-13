12e28a05e6b3ab14082f9e0954c8b0569d18f2a1
ethanryan <ethanryan@gmail.com>
2018-10-12 21:57:24 -0400

    radio buttons working with defaultChecked conditional for colorSelected in EditForm

--- src/components/EditForm.js
+++ src/components/EditForm.js
@@ -15,21 +15,54 @@ class EditForm extends Component {
 
           <label>
             Color:
-            <input type="text"
-              name="colorSelected"
-              onChange={this.props.handleInputChange}
-            />
+
+            {/* <div>
+              <input type="radio" id="contactChoice1"
+               name="contact" value="email"/>
+              <label>Email</label>
+
+              <input type="radio" id="contactChoice2"
+               name="contact" value="phone"/>
+              <label>Phone</label>
+
+              <input type="radio" id="contactChoice3"
+               name="contact" value="mail"/>
+              <label>Mail</label>
+            </div> */}
+
+            <div>
+              <input type="radio" id="contactChoice1"
+                onChange={this.props.handleInputChange}
+                name="colorSelected" value="red" defaultChecked={(this.props.item.colorSelected === "red") ? true : false}
+              />
+              <label>red</label>
+
+              <input type="radio" id="contactChoice2"
+                onChange={this.props.handleInputChange}
+               name="colorSelected" value="green" defaultChecked={(this.props.item.colorSelected === "green") ? true : false}
+             />
+              <label>green</label>
+
+              <input type="radio" id="contactChoice3"
+                onChange={this.props.handleInputChange}
+               name="colorSelected" value="blue" defaultChecked={(this.props.item.colorSelected === "blue") ? true : false}
+             />
+              <label>blue</label>
+            </div>
+
+            {/* <select name="colorSelected"
+              defaultValue={this.props.item.colorSelected}
+              onChange={this.props.handleInputChange}>
+              <option value="red">red</option>
+              <option value="blue">blue</option>
+              <option value="pink">pink</option>
+            </select> */}
           </label>
 
       <br></br>
 
           <label>
             Size:
-            {/* <input type="text"
-              name="sizeSelected"
-              defaultValue={this.props.item.sizeSelected}
-              onChange={this.props.handleInputChange}
-            /> */}
             <select name="sizeSelected"
               defaultValue={this.props.item.sizeSelected}
               onChange={this.props.handleInputChange}>
